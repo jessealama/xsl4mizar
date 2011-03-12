ea47aba36312b8137b7cd9a5070fed9efff0d741
Jesse Alama <jesse.alama@gmail.com>
2011-03-12 13:39:51 +0000

    Cleanup using JSlint.
    http://www.jslint.com/ .

--- MHTML/mhtml.js
+++ MHTML/mhtml.js
@@ -1,9 +1,9 @@
 function hs(obj)
 {
 // document.getElementById('myimage').nextSibling.style.display = 'block';
-if (obj.nextSibling.style.display == 'inline')
+if (obj.nextSibling.style.display === 'inline')
  { obj.nextSibling.style.display = 'none'; }
-else { if (obj.nextSibling.style.display == 'none')
+else { if (obj.nextSibling.style.display === 'none')
  { obj.nextSibling.style.display = 'inline'; }
  else { obj.nextSibling.style.display = 'inline';  }}
 return false;
@@ -11,9 +11,9 @@ return false;
 
 function hs2(obj)
 {
-if (obj.nextSibling.style.display == 'block')
+if (obj.nextSibling.style.display === 'block')
  { obj.nextSibling.style.display = 'none'; }
-else { if (obj.nextSibling.style.display == 'none')
+else { if (obj.nextSibling.style.display === 'none')
  { obj.nextSibling.style.display = 'block'; }
  else { obj.nextSibling.style.display = 'none';  }}
 return false;
@@ -21,9 +21,24 @@ return false;
 function hsNdiv(obj)
 {
 var ndiv = obj;
-while (ndiv.nextSibling.nodeName != 'DIV') { ndiv = ndiv.nextSibling; }
+    while (ndiv.nextSibling.nodeName  !== 'DIV') { ndiv = ndiv.nextSibling; }
 return hs2(ndiv);
 }
+
+// commented the 200 state to have local requests too
+function insertRequest(obj,http_request) {
+        if (http_request.readyState === 4) {
+//            if (http_request.status == 200) {
+	    var ndiv = obj;
+	    while (ndiv.nodeName !== 'SPAN') { ndiv = ndiv.nextSibling; }
+	    ndiv.innerHTML = http_request.responseText;
+	    obj.onclick = function(){ return hs2(obj); };
+//            } else {
+//                alert('There was a problem with the request.');
+//		alert(http_request.status);
+//            }
+	    }}
+//
 
 // explorer7 implements XMLHttpRequest in some strange way
 function makeRequest(obj,url) {
@@ -36,10 +51,10 @@ function makeRequest(obj,url) {
         } else if (window.ActiveXObject) { // IE
             try {
                 http_request = new ActiveXObject('Msxml2.XMLHTTP');
-            } catch (e) {
+            } catch (e1) {
                 try {
                     http_request = new ActiveXObject('Microsoft.XMLHTTP');
-                } catch (e) {}
+                } catch (e2) { }
             }
         }
         if (!http_request) {
@@ -50,17 +65,3 @@ function makeRequest(obj,url) {
         http_request.open('GET', url, true);
         http_request.send(null);
     }
-// commented the 200 state to have local requests too
-function insertRequest(obj,http_request) {
-        if (http_request.readyState == 4) {
-//            if (http_request.status == 200) {
-	    var ndiv = obj;
-	    while (ndiv.nodeName != 'SPAN') { ndiv = ndiv.nextSibling; }
-	    ndiv.innerHTML = http_request.responseText;
-	    obj.onclick = function(){ return hs2(obj) };
-//            } else {
-//                alert('There was a problem with the request.');
-//		alert(http_request.status);
-//            }
-	    }}
-//
