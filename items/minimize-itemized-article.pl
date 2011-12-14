72186657f28a47ed396da433a70e0b6cac0aa3c7
Jesse Alama <jesse.alama@gmail.com>
2011-12-14 18:34:55 +0100

    Use Getopt::Long, and support two options: verbose operation, and minimization of the original article.

--- items/minimize-itemized-article.pl
+++ items/minimize-itemized-article.pl
@@ -1,9 +1,81 @@
 #!/usr/bin/perl -w
 
 use strict;
-
 use File::Basename qw(basename dirname);
 use File::Copy qw(copy move);
+use Getopt::Long;
+use Pod::Usage;
+
+=cut
+
+=head1 minimize-itemized-article.pl
+
+minimize-itemized-article.pl - Minimize the fragments of an "itemized" mizar article
+
+=head1 SYNOPSIS
+
+minimize-itemized-article.pl [options] itemized-mizar-article-directory
+
+Options:
+
+  -help                       Brief help message
+
+  -man                        Full documentation
+
+  -verbose                    Say what we're doing
+
+  -minimize-whole-article     Minimize the whole article and use its minimal environment in minimizing its fragments
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--help>
+
+Prints a brief help message and exits.
+
+=item B<--man>
+
+Prints the manual page and exits.
+
+=item B<--verbose>
+
+Say what environment file we're minimizing, and for each environment
+file, say how many environment "items" are present there and how many
+we really need.
+
+=item B<--minimize-whole-article>
+
+Before minimizing the environment of each of the items from an
+article, minimize the whole article and use its minimal environment as
+a basis for each of the article's item's environment.
+
+Such minimization may an expensive up-front cost, but it can save time
+in the long run, depending on the size of the original article's
+environment and how expensive it is to verify it.
+
+=back
+
+=head1 DESCRIPTION
+
+B<minimize-itemized-article.pl> will construct, for each fragment of
+an itemized mizar article, the smallest environment with respect to
+which the given article is verifiable.
+
+=cut
+
+my $verbose = 0;
+my $man = 0;
+my $help = 0;
+my $minimize_whole_article = 0;
+
+GetOptions('help|?' => \$help,
+           'man' => \$man,
+           'verbose'  => \$verbose,
+           'minimize-whole-article' => \$minimize_whole_article)
+  or pod2usage(2);
+pod2usage(1) if $help;
+pod2usage(-exitstatus => 0, -verbose => 2) if $man;
 
 if (scalar @ARGV != 1) {
   print 'Usage: minimize-itemized-article.pl ITEMIZED-ARTICLE-DIRECTORY', "\n";
@@ -67,16 +139,28 @@ unless (-r $prefer_environment_stylesheet) {
   exit 1;
 }
 
-# Minimize the original article
 
-print 'Minimizing the itemized article...';
+if ($minimize_whole_article == 1) {
 
-my $minimize_status = system ("$minimize_script --verbose $itemized_article_miz");
-my $minimize_exit_code = $minimize_status >> 8;
+  if ($verbose == 1) {
+    print 'Minimizing the itemized article...', "\n";
+  }
 
-if ($minimize_exit_code != 0) {
-  print 'Error: minimization of the itemized article ', $itemized_article_miz, ' did not exit cleanly.', "\n";
-  exit 1;
+  my $minimize_call =
+    ($verbose == 1) ? "$minimize_script --verbose $itemized_article_miz"
+                    : "$minimize_script $itemized_article_miz";
+
+  my $minimize_status = system ($minimize_call);
+  my $minimize_exit_code = $minimize_status >> 8;
+
+  if ($minimize_exit_code != 0) {
+    print 'Error: minimization of the itemized article ', $itemized_article_miz, ' did not exit cleanly.', "\n";
+    exit 1;
+  }
+
+  if ($verbose == 1) {
+    print 'Done minimizing the itemized article.', "\n";
+  }
 }
 
 my @fragments = `find $article_text_dir -name "ckb*.miz"`;
