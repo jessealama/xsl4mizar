932414f884f84ea169bf670f0189a86c3717cf69
Jesse Alama <jesse.alama@gmail.com>
2011-12-21 17:06:30 +0100

    Use a better basename for the temporary directory.

--- items/minimize-itemized-article.pl
+++ items/minimize-itemized-article.pl
@@ -233,7 +233,7 @@ unless (-r $prefer_environment_stylesheet) {
 
 my $real_workdir = undef;
 if (defined $workdir) {
-  $real_workdir = tempdir ('minimizationXXXX',
+  $real_workdir = tempdir ("minimization-$article_basename-XXXX",
 			   CLEANUP => 1,
 			   DIR => $workdir);
   if ($verbose == 1) {
