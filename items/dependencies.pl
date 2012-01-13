6758dc18ffa892dc70b20885ac7ecd05c7b66940
Jesse Alama <jesse.alama@gmail.com>
2012-01-13 04:06:54 +0100

    Use die.
    Even though I don't like it.

--- items/dependencies.pl
+++ items/dependencies.pl
@@ -28,13 +28,11 @@ my $article_xml = "${article_dirname}/${article_basename}.xml";
 my $article_absolute_xml = "${article_dirname}/${article_basename}.xml1";
 
 unless (-e $stylesheet_home) {
-  print 'Error: the supplied directory', "\n", "\n", '  ', $stylesheet_home, "\n", "\n", 'in which we look for stylesheets does not exist.', "\n";
-  exit 1;
+  die 'Error: the supplied directory', "\n", "\n", '  ', $stylesheet_home, "\n", "\n", 'in which we look for stylesheets does not exist.', "\n";
 }
 
 unless (-d $stylesheet_home) {
-  print 'Error: the supplied directory', "\n", "\n", '  ', $stylesheet_home, "\n", "\n", 'in which we look for stylesheets is not actually a directory.', "\n";
-  exit 1;
+  die 'Error: the supplied directory', "\n", "\n", '  ', $stylesheet_home, "\n", "\n", 'in which we look for stylesheets is not actually a directory.', "\n";
 }
 
 my %stylesheet_paths = ('absrefs' => "${stylesheet_home}/addabsrefs.xsl",
@@ -44,18 +42,15 @@ my %stylesheet_paths = ('absrefs' => "${stylesheet_home}/addabsrefs.xsl",
 foreach my $stylesheet (keys %stylesheet_paths) {
   my $stylesheet_path = $stylesheet_paths{$stylesheet};
   unless (-e $stylesheet_path) {
-    print 'Error: the ', $stylesheet, ' stylesheet does not exist at the expected location (', $stylesheet_path, ').', "\n";
-    exit 1;
+    die 'Error: the ', $stylesheet, ' stylesheet does not exist at the expected location (', $stylesheet_path, ').';
   }
   unless (-r $stylesheet_path) {
-    print 'Error: the ', $stylesheet, ' stylesheet at ', $stylesheet_path, ' is unreadable.', "\n";
-    exit 1;
+    die 'Error: the ', $stylesheet, ' stylesheet at ', $stylesheet_path, ' is unreadable.';
   }
 }
 
 if (! -e $article_xml) {
-  print 'The XML for ', $article_basename, ' does not exist.';
-  exit 1;
+  die 'The XML for ', $article_basename, ' does not exist.';
 }
 
 if (! -e $article_absolute_xml) {
@@ -71,7 +66,7 @@ if (! -e $article_absolute_xml) {
 
   unless (-e $article_absolute_xml) {
     print "\n";
-    print 'Error: we failed to generate the absolute form of the XML for ', $article_basename, '.', "\n";
+    die 'Error: we failed to generate the absolute form of the XML for ', $article_basename, '.', "\n";
     exit 1;
   }
 
@@ -84,8 +79,7 @@ chomp @non_constructor_deps;
 
 # Sanity check: the stylsheet didn't produce any junk output
 if (grep (/^$/, @non_constructor_deps)) {
-  print 'Error: the dependencies stylesheet generated some junk output (a blank line).', "\n";
-  exit 1;
+  die 'Error: the dependencies stylesheet generated some junk output (a blank line).';
 }
 
 # Constructors are a special case
@@ -96,8 +90,7 @@ chomp @constructor_deps;
 
 # Sanity check: the stylesheet didn't produce any junk output
 if (grep (/^$/, @constructor_deps)) {
-  print 'Error: the inferred-constructors stylesheet generated some junk output.', "\n";
-  exit 1;
+  die 'Error: the inferred-constructors stylesheet generated some junk output.';
 }
 
 foreach my $dep (@constructor_deps, @non_constructor_deps) {
