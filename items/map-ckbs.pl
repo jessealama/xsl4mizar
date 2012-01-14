6a4199890484efb8496691712a2223373c56d3fd
Jesse Alama <jesse.alama@gmail.com>
2012-01-14 02:02:43 +0100

    Documentation, XML parser, etc.

--- items/map-ckbs.pl
+++ items/map-ckbs.pl
@@ -3,27 +3,40 @@
 use strict;
 use File::Basename qw(basename);
 use XML::LibXML;
+use Getopt::Long;
+use Pod::Usage;
 
-unless (scalar @ARGV == 1) {
-  print 'Usage: map-ckbs.pl ITEMIZED-ARTICLE-DIRECTORY', "\n";
-  exit 1;
-}
+my $help = 0;
+my $man = 0;
+my $verbose = 0;
+
+GetOptions('help|?' => \$help,
+           'man' => \$man,
+           'verbose'  => \$verbose)
+  or pod2usage(2);
+pod2usage(1) if $help;
+pod2usage(-exitstatus => 0, -verbose => 2) if $man;
+pod2usage(1) if (scalar @ARGV != 1);
 
 my $article_dir = $ARGV[0];
 
-unless (-d $article_dir) {
-  print 'Error: ', $article_dir, ' is not a directory.', "\n";
-  exit 1;
+if (! -d $article_dir) {
+  die 'Error: ', $article_dir, ' is not a directory.', "\n";
 }
 
 my $article_basename = basename ($article_dir);
 
 my $prel_subdir = "${article_dir}/prel";
 
-unless (-d $prel_subdir) {
-  print 'Error: the prel subdirectory of ', $article_dir, ' does not exist.', "\n";
-  exit 1;
+if (! -e $prel_subdir) {
+  die 'Error: the prel subdirecory of ', $article_dir, ' does not exist.';
 }
+
+if (! -d $prel_subdir) {
+  die 'Error: the prel subdirectory of ', $article_dir, ' does not exist (as a directory).';
+}
+
+my $xml_parser = XML::LibXML->new();
 
 # Constructors
 
@@ -35,13 +48,23 @@ my %constructors = ();
 foreach my $i (1 .. scalar @dcos) {
   my $dco = $dcos[$i - 1];
   my $dco_path = "$prel_subdir/ckb${dco}.dco";
-  my $constructor_line = `grep '<Constructor ' $dco_path`;
-  chomp $constructor_line;
-  $constructor_line =~ m/ kind=\"(.)\"/;
-  my $kind = $1;
-  unless (defined $kind) {
-    print 'Error: we failed to extract a kind attribute from the Constructor XML element', "\n", "\n", '  ', $constructor_line, "\n";
+  my $dco_doc = undef;
+  if (! eval { $dco_doc = $xml_parser->parse_file ($dco_path) } ) {
+    print STDERR 'Error: the .dco file at ', $dco_path, ' is not well-formed XML.';
   }
+  my @constructors = $dco_doc->findnodes ('Constructors/Constructor');
+  if (scalar @constructors == 0) {
+    print STDERR ('Error: we found no Constructor nodes in ', $dco_path, '.');
+  }
+  if (scalar @constructors > 1) {
+    print STDERR ('Error: we found multiple Constructor nodes in ', $dco_path, '.');
+  }
+  my $constructor = $constructors[0];
+  if (! $constructor->exists ('@kind')) {
+    print STDERR ('Error: we failed to extract a kind attribute from a Constructor XML element in ', $dco_path, '.', "\n");
+    next;
+  }
+  my $kind = $constructor->findvalue ('@kind');
   my $kind_lc = lc $kind;
   my $num;
   if (defined $constructors{$kind}) {
@@ -64,13 +87,24 @@ my %definiens = ();
 foreach my $i (1 .. scalar @defs) {
   my $def = $defs[$i - 1];
   my $def_path = "$prel_subdir/ckb${def}.def";
-  my $definiens_line = `grep '<Definiens ' $def_path`;
-  chomp $definiens_line;
-  $definiens_line =~ m/ constrkind=\"(.)\"/;
-  my $constrkind = $1;
-  unless (defined $constrkind) {
-    print 'Error: we failed to extract a constrkind attribute from the Definiens XML element', "\n", "\n", '  ', $definiens_line, "\n";
+  my $def_doc = undef;
+
+  if (! eval { $def_doc = $xml_parser->parse_file ($def_path) } ) {
+    print STDERR 'Error: the .def file at ', $def_path, ' is not well-formed XML.';
   }
+  my @definientia = $def_doc->findnodes ('Definientia/Definiens');
+  if (scalar @definientia == 0) {
+    print STDERR ('Error: we found no Definiens nodes in ', $def_path, '.');
+  }
+  if (scalar @definientia > 1) {
+    print STDERR ('Error: we found multiple Definiens nodes in ', $def_path, '.');
+  }
+  my $definiens = $definientia[0];
+  if (! $definiens->exists ('@constrkind')) {
+    print STDERR ('Error: we failed to extract a constrkind attribute from a Definiens XML element in ', $def_path, '.', "\n");
+    next;
+  }
+  my $constrkind = $definiens->findvalue ('@constrkind');
   my $constrkind_lc = lc $constrkind;
   my $num;
   if (defined $definiens{$constrkind}) {
@@ -82,7 +116,6 @@ foreach my $i (1 .. scalar @defs) {
   }
   print $article_basename, ':', $constrkind_lc, 'definiens', ':', $num, ' => ', $article_basename, ':', 'fragment', ':', $def, "\n";
 }
-
 
 # Deftheorems
 
@@ -123,9 +156,8 @@ foreach my $i (1 .. scalar @dcls) {
   chomp $cluster_line;
   $cluster_line =~ m/([CFR])Cluster /;
   my $kind = $1;
-  unless (defined $kind) {
-    print 'Error: we failed to extract the cluster kind from the XML element', "\n", "\n", '  ', $cluster_line, "\n";
-    exit 1;
+  if (! defined $kind) {
+    print STDERR ('Error: we failed to extract the cluster kind from the XML element', "\n", "\n", '  ', $cluster_line, "\n");
   }
   my $kind_lc = lc $kind;
   my $num;
@@ -141,8 +173,6 @@ foreach my $i (1 .. scalar @dcls) {
 
 # Notations
 
-# Theorems and deftheorems
-
 my @dnos = `find $prel_subdir -name "*.dno" -exec basename {} .dno ';' | sed -e 's/ckb//' | sort --numeric-sort`;
 chomp @dnos;
 
@@ -155,8 +185,8 @@ foreach my $i (1 .. scalar @dnos) {
   chomp $pattern_line;
   $pattern_line =~ m/ constrkind=\"(.)\"/;
   my $constrkind = $1;
-  unless (defined $constrkind) {
-    print 'Error: we failed to extract a kind attribute from dno Pattern XML element', "\n", "\n", '  ', $pattern_line, "\n";
+  if (! defined $constrkind) {
+    print STDERR ('Error: we failed to extract a kind attribute from dno Pattern XML element', "\n", "\n", '  ', $pattern_line, "\n");
   }
   my $constrkind_lc = lc $constrkind;
   my $num;
@@ -174,9 +204,8 @@ foreach my $i (1 .. scalar @dnos) {
 
 my $article_wsx = "${article_dir}/${article_basename}.wsx";
 
-unless (-e $article_wsx) {
-  print 'Error: the .wsx for ', $article_basename, ' does not exist at the expected location (', $article_wsx, ').', "\n";
-  exit 1;
+if (! -e $article_wsx) {
+  die 'Error: the .wsx for ', $article_basename, ' does not exist at the expected location (', $article_wsx, ').';
 }
 
 # Create the split-and-itemized wsx
@@ -186,17 +215,15 @@ my $article_wsx_split_itemized = "${article_dir}/${article_basename}.wsxsi";
 my $split_stylesheet = '/Users/alama/sources/mizar/xsl4mizar/items/split.xsl';
 my $itemize_stylesheet = '/Users/alama/sources/mizar/xsl4mizar/items/itemize.xsl';
 
-unless (-e $article_wsx_split_itemized) {
+if (! -e $article_wsx_split_itemized) {
   my $xsltproc_status =
     system ("xsltproc $split_stylesheet $article_wsx | xsltproc --output $article_wsx_split_itemized $itemize_stylesheet -");
   my $xsltproc_exit_code = $xsltproc_status >> 8;
   if ($xsltproc_exit_code != 0) {
-    print 'Error: something went wrong creating the split-and-itemized wsx for ', $article_basename, '.', "\n";
-    exit 1;
+    die 'Error: something went wrong creating the split-and-itemized wsx for ', $article_basename, '.';
   }
 }
 
-my $xml_parser = XML::LibXML->new();
 my $wsx_doc = $xml_parser->parse_file ("$article_wsx_split_itemized");
 
 my @theorems = $wsx_doc->findnodes ('Fragments/Text-Proper/Item[@kind = "Theorem-Item" and @promoted-lemma = "no"]');
@@ -210,8 +237,7 @@ foreach my $theorem (@theorems) {
     my $fragment_number = $theorem->findvalue ('count (preceding::Text-Proper) + 1');
     print $article_basename, ':', 'theorem', ':', $theorem_number, ' => ', $article_basename, ':', 'fragment', ':', $fragment_number, "\n";
   } else {
-    print 'Error: we did not find the theorem-number attribute for a theorem!', "\n";
-    exit 1;
+    die 'Error: we did not find the theorem-number attribute for a theorem!', "\n";
   }
 }
 
@@ -228,8 +254,7 @@ foreach my $lemma (@lemmas) {
     my $fragment_number = $lemma->findvalue ('count (preceding::Text-Proper) + 1');
     print $article_basename, ':', 'lemma', ':', $lemma_number, ' => ', $article_basename, ':', 'fragment', ':', $fragment_number, "\n";
   } else {
-    print 'Error: we did not find the lemma-number attribute for a promoted lemma!', "\n";
-    exit 1;
+    die 'Error: we did not find the lemma-number attribute for a promoted lemma!';
   }
 }
 
@@ -246,7 +271,63 @@ foreach my $lemma (@unexportable_lemmas) {
     my $fragment_number = $lemma->findvalue ('count (preceding::Text-Proper) + 1');
     print $article_basename, ':', 'lemma', ':', $lemma_number, ' => ', $article_basename, ':', 'fragment', ':', $fragment_number, "\n";
   } else {
-    print 'Error: we did not find the lemma-number attribute for an unpromoted lemma!', "\n";
-    exit 1;
+    die 'Error: we did not find the lemma-number attribute for an unpromoted lemma!', "\n";
   }
 }
+
+__END__
+
+=head1 MAP-CKBS
+
+map-ckbs.pl - Resolve names of article-internal items ("CKBs")
+
+=head1 SYNOPSIS
+
+map-ckbs.pl [options] directory
+
+Interpret the supplied directory as the result of itemizing a Mizar
+article.  Print a mapping that shows how each of the items in the
+itemized article is resolved into its absolute name.
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--help>
+
+Prints a brief help message and exits.
+
+=item B<--man>
+
+Prints the manual page and exits.
+
+=item B<--verbose>
+
+Say what we're doing as we're doing it.
+
+=back
+
+=head1 DESCRIPTION
+
+B<map-ckbs.pl> will consult the given article directory to determine
+how any "article-internal" items are resolved to their absolute names.
+
+=head1 REQUIRED ARGUMENTS
+
+It is necessary to supply a directory as the one and only argument of
+this program.  The directory is supposed to be the result of itemizing
+a Mizar article.  It should have the structure of a multi-article
+Mizar development: there should be subdirectories 'prel', 'dict', and
+'text'.
+
+=head1 SEE ALSO
+
+=over 8
+
+=item F<itemized-article-dependencies.pl>
+
+=item L<http://mizar.org/>
+
+=back
+
+=cut
