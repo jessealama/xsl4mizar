1362cb5116ea25f0653df6136b89edc50635b517
Jesse Alama <jesse.alama@gmail.com>
2012-01-15 11:52:33 +0100

    Use croak.

--- items/minimize-lemmas.pl
+++ items/minimize-lemmas.pl
@@ -8,6 +8,7 @@ use Getopt::Long;
 use Pod::Usage;
 use Cwd qw(cwd);
 use File::Copy qw(copy move);
+use Carp qw(croak carp);
 
 my $paranoid = 0;
 my $verbose = 0;
@@ -36,12 +37,13 @@ pod2usage(1) unless (scalar @ARGV == 1);
 ######################################################################
 
 unless (-e $stylesheet_home) {
-  die 'Error: the supplied directory', "\n", "\n", '  ', $stylesheet_home, "\n", "\n", 'in which we look for stylesheets does not exist.';
+  croak ('Error: the supplied directory', "\n", "\n", '  ', $stylesheet_home, "\n", "\n", 'in which we look for stylesheets does not exist.');
 }
 
 unless (-d $stylesheet_home) {
-  die 'Error: the supplied directory', "\n", "\n", '  ', $stylesheet_home, "\n", "\n", 'in which we look for stylesheets is not actually a directory.';
+  croak ('Error: the supplied directory', "\n", "\n", '  ', $stylesheet_home, "\n", "\n", 'in which we look for stylesheets is not actually a directory.');
 }
+
 
 my %stylesheet_paths =
   ('truncate' => "${stylesheet_home}/truncate.xsl",
@@ -73,54 +75,54 @@ my $rewrite_aid_stylesheet = path_for_stylesheet ('rewrite-aid');
 foreach my $stylesheet (keys %stylesheet_paths) {
   my $stylesheet_path = path_for_stylesheet ($stylesheet);
   unless (-e $stylesheet_path) {
-    die 'Error: the ', $stylesheet, ' stylesheet does not exist at the expected location (', $stylesheet_path, ').';
+    croak ('Error: the ', $stylesheet, ' stylesheet does not exist at the expected location (', $stylesheet_path, ').');
   }
   unless (-r $stylesheet_path) {
-    die 'Error: the ', $stylesheet, ' stylesheet at ', $stylesheet_path, ' is unreadable.';
+    croak ('Error: the ', $stylesheet, ' stylesheet at ', $stylesheet_path, ' is unreadable.');
   }
 }
 
 unless (-e $script_home) {
-  die 'Error: the supplied directory', "\n", "\n", '  ', $script_home, "\n", "\n", 'in which we look for scripts does not exist.';
+  croak ('Error: the supplied directory', "\n", "\n", '  ', $script_home, "\n", "\n", 'in which we look for scripts does not exist.');
 }
 
 unless (-d $script_home) {
-  die 'Error: the supplied directory', "\n", "\n", '  ', $script_home, "\n", "\n", 'in which we look for scripts is not actually a directory.';
+  croak ('Error: the supplied directory', "\n", "\n", '  ', $script_home, "\n", "\n", 'in which we look for scripts is not actually a directory.');
 }
 
 my $brutalize_script = "${script_home}/minimal.pl";
 my $brutalize_internally_script = "${script_home}/minimize-internally.pl";
 
 if (! -e $brutalize_script) {
-  die 'Error: the minimization script could not be found at the expected location (', $brutalize_script, ').';
+  croak ('Error: the minimization script could not be found at the expected location (', $brutalize_script, ').');
 }
 
 if (! -f $brutalize_script) {
-  die 'Error: the minimization script at ', $brutalize_script, ' is not a file.';
+  croak ('Error: the minimization script at ', $brutalize_script, ' is not a file.');
 }
 
 if (! -r $brutalize_script) {
-  die 'Error: the minimization script at ', $brutalize_script, ' is not readable.';
+  croak ('Error: the minimization script at ', $brutalize_script, ' is not readable.');
 }
 
 if (! -x $brutalize_script) {
-  die 'Error: the minimization script at ', $brutalize_script, ' is not executable.';
+  croak ('Error: the minimization script at ', $brutalize_script, ' is not executable.');
 }
 
 if (! -e $brutalize_internally_script) {
-  die 'Error: the minimization script could not be found at the expected location (', $brutalize_internally_script, ').';
+  croak ('Error: the minimization script could not be found at the expected location (', $brutalize_internally_script, ').');
 }
 
 if (! -f $brutalize_internally_script) {
-  die 'Error: the minimization script at ', $brutalize_internally_script, ' is not a file.';
+  croak ('Error: the minimization script at ', $brutalize_internally_script, ' is not a file.');
 }
 
 if (! -r $brutalize_internally_script) {
-  die 'Error: the minimization script at ', $brutalize_internally_script, ' is not readable.';
+  croak ('Error: the minimization script at ', $brutalize_internally_script, ' is not readable.');
 }
 
 if (! -x $brutalize_internally_script) {
-  die 'Error: the minimization script at ', $brutalize_internally_script, ' is not executable.';
+  croak ('Error: the minimization script at ', $brutalize_internally_script, ' is not executable.');
 }
 
 my $article = $ARGV[0];
@@ -130,20 +132,19 @@ my $article_sans_extension = "${article_dirname}/${article_basename}";
 
 if (defined $target_directory) {
   if (-e $target_directory) {
-    die 'Error: the supplied target directory, ', $target_directory, ' already exists.  Please move it out of the way.';
+    croak ('Error: the supplied target directory, ', $target_directory, ' already exists.  Please move it out of the way.');
   } else {
     mkdir $target_directory
-      or die 'Error: unable to make the directory \'', $target_directory, '\'.';
+      or croak ('Error: unable to make the directory \'', $target_directory, '\'.');
   }
 } else {
   my $cwd = cwd ();
   $target_directory = "${cwd}/${article_basename}";
   if (-e $target_directory) {
-    die 'Error: since the target-directory option was not used, we are to save our wok in \'', $article_basename, '\'; but there is already a directory by that name in the current working directory.  Please move it out of the way.';
-    exit 1;
+    croak ('Error: since the target-directory option was not used, we are to save our wok in \'', $article_basename, '\'; but there is already a directory by that name in the current working directory.  Please move it out of the way.');
   }
   mkdir $target_directory
-    or die 'Error: unable to make the directory \'', $article_basename, '\' in the current working directory.';
+    or croak ('Error: unable to make the directory \'', $article_basename, '\' in the current working directory.');
 }
 
 # Copy the article
@@ -151,42 +152,42 @@ if (defined $target_directory) {
 my $article_miz = "${article_dirname}/${article_basename}.miz";
 
 if (! -e $article_miz) {
-  die 'Error: ', $article_miz, ' does not exist.';
+  croak ('Error: ', $article_miz, ' does not exist.');
 }
 
 if (! -r $article_miz) {
-  die 'Error: ', $article_miz, ' is unreadable.';
+  croak ('Error: ', $article_miz, ' is unreadable.');
 }
 
 my $article_err_in_target_dir = "${target_directory}/${article_basename}.err";
 my $article_miz_in_target_dir = "${target_directory}/${article_basename}.miz";
 
 copy ($article_miz, $article_miz_in_target_dir)
-  or die 'Error: unable to copy the article at ', $article_miz, ' to ', $article_miz_in_target_dir, '.';
+  or croak ('Error: unable to copy the article at ', $article_miz, ' to ', $article_miz_in_target_dir, '.');
 
 chdir $target_directory;
 
 my $accom_status = system ("accom -l -q $article_miz_in_target_dir > /dev/null 2>&1");
 my $accom_exit_code = $accom_status >> 8;
 if ($accom_exit_code != 0) {
-  die 'Error: unable to accommodate ', $article_basename, ' in the target directory (', $target_directory, ').';
+  croak ('Error: unable to accommodate ', $article_basename, ' in the target directory (', $target_directory, ').');
 }
 
 my $verifier_status = system ("verifier -l -q $article_miz_in_target_dir > /dev/null 2>&1");
 my $verifier_exit_code = $verifier_status >> 8;
 if ($verifier_exit_code != 0) {
-  die 'Error: unable to verify ', $article_basename, ' in the target directory (', $target_directory, ').';
+  croak ('Error: unable to verify ', $article_basename, ' in the target directory (', $target_directory, ').');
 }
 
 if (! -z $article_err_in_target_dir) {
-  die 'Error: verifier produced a non-empty error file for ', $article_basename, ' in the target directory (', $target_directory, ').';
+  croak ('Error: verifier produced a non-empty error file for ', $article_basename, ' in the target directory (', $target_directory, ').');
 }
 
 my $article_xml_in_target_dir = "${target_directory}/${article_basename}.xml";
 my $article_absolute_xml_in_target_dir = "${target_directory}/${article_basename}.xml1";
 
 if (! -e $article_xml_in_target_dir) {
-  die 'Error: verifier did not produce a semantic XML representation of ', $article_basename, '.';
+  croak ('Error: verifier did not produce a semantic XML representation of ', $article_basename, '.');
 }
 
 foreach my $extension ('xml', 'eno', 'dfs', 'ecl', 'eid', 'epr', 'erd', 'esh', 'eth') {
@@ -196,7 +197,7 @@ foreach my $extension ('xml', 'eno', 'dfs', 'ecl', 'eid', 'epr', 'erd', 'esh', '
     my $xsltproc_absrefs_status = system ("xsltproc --output $target_xml_file $absrefs_stylesheet $source_xml_file 2>/dev/null");
     my $xsltproc_absrefs_exit_code = $xsltproc_absrefs_status >> 8;
     if ($xsltproc_absrefs_exit_code != 0) {
-      die 'Error: xsltproc did not exit cleanly when computing the absolute form of ', $article_basename, '.', $extension, '; its exit code was ', $xsltproc_absrefs_exit_code, '.';
+      croak ('Error: xsltproc did not exit cleanly when computing the absolute form of ', $article_basename, '.', $extension, '; its exit code was ', $xsltproc_absrefs_exit_code, '.');
     }
   }
 }
@@ -212,7 +213,7 @@ foreach my $element_and_proposition (@elements_and_propositions) {
     = split (/ /, $element_and_proposition);
   if (defined $element_position) {
     if (scalar @generated_propositions == 0) {
-      die 'Error: unable to parse the line \'', $element_and_proposition, '\' coming from the application of the toplevel-propositions stylesheet to the absolute XML representation of ', $article_basename, '.';
+      croak ('Error: unable to parse the line \'', $element_and_proposition, '\' coming from the application of the toplevel-propositions stylesheet to the absolute XML representation of ', $article_basename, '.');
     } else {
       $propositions_for_element{$element_position} = \@generated_propositions;
       foreach my $prop (@generated_propositions) {
@@ -220,22 +221,22 @@ foreach my $element_and_proposition (@elements_and_propositions) {
       }
     }
   } else {
-    die 'Error: unable to parse the line \'', $element_and_proposition, '\' coming from the application of the toplevel-propositions stylesheet to the absolute XML representation of ', $article_basename, '.';
+    croak ('Error: unable to parse the line \'', $element_and_proposition, '\' coming from the application of the toplevel-propositions stylesheet to the absolute XML representation of ', $article_basename, '.');
   }
 }
 
 opendir (DIR, $target_directory)
-  or die 'Error: unable to open the target directory at ', $target_directory, '.';
+  or croak ('Error: unable to open the target directory at ', $target_directory, '.');
 my @mizar_generated_files = readdir DIR;
 closedir DIR
-  or die 'Error: unable to close the directory at ', $target_directory, '.';
+  or croak ('Error: unable to close the directory at ', $target_directory, '.');
 
 sub extension {
   my $path = shift;
   if ($path =~ /[.]([^.]+)$/) {
     return $1;
   } else {
-    die 'Error: the path \'', $path, '\' does not have an extension.';
+    croak ('Error: the path \'', $path, '\' does not have an extension.');
   }
 }
 
@@ -246,7 +247,7 @@ foreach my $lemma (keys %element_for_proposition) {
       my $ext = extension ($mizar_file);
       my $lemma_file = "${lemma}.${ext}";
       copy ($mizar_file, $lemma_file)
-	or die 'Error: unable to copy the original Mizar file ', $mizar_file, ' to its lemma clone ', $lemma_file, '.';
+	or croak ('Error: unable to copy the original Mizar file ', $mizar_file, ' to its lemma clone ', $lemma_file, '.');
     }
   }
 }
@@ -261,10 +262,10 @@ foreach my $lemma (keys %element_for_proposition) {
   my $xsltproc_truncate_status = system ("xsltproc --output '$lemma_xml_temp' --stringparam after '$lemma_position' $truncate_stylesheet $lemma_xml 2>/dev/null");
   my $xsltproc_truncate_exit_code = $xsltproc_truncate_status >> 8;
   if ($xsltproc_truncate_exit_code != 0) {
-    die 'Error: xsltproc did not exit cleanly when applying the truncate stylesheet to ', $lemma_xml, '.';
+    croak ('Error: xsltproc did not exit cleanly when applying the truncate stylesheet to ', $lemma_xml, '.');
   }
   move ($lemma_xml_temp, $lemma_xml)
-    or die 'Error: unable to move the temporary lemma XML at ', $lemma_xml_temp, ' to ', $lemma_xml, '.';
+    or croak ('Error: unable to move the temporary lemma XML at ', $lemma_xml_temp, ' to ', $lemma_xml, '.');
 }
 
 # If we're in paranoid mode, check that new XML is verifiable
@@ -281,7 +282,7 @@ if ($paranoid) {
       if ($verbose) {
 	print "\n";
       }
-      die 'Error: lemma ', $lemma, ' is not verifiable!';
+      croak ('Error: lemma ', $lemma, ' is not verifiable!');
     }
   }
   if ($verbose) {
@@ -299,10 +300,10 @@ foreach my $lemma (keys %element_for_proposition) {
   my $xsltproc_skip_status = system ("xsltproc --output '$lemma_xml_temp' $skip_non_ultimate_proofs_stylesheet $lemma_xml");
   my $xsltproc_skip_exit_code = $xsltproc_skip_status >> 8;
   if ($xsltproc_skip_exit_code != 0) {
-    die 'Error: xsltproc did not exit cleanly when applying the skip-proofs stylesheet to ', $lemma_xml, '.';
+    croak ('Error: xsltproc did not exit cleanly when applying the skip-proofs stylesheet to ', $lemma_xml, '.');
   }
   move ($lemma_xml_temp, $lemma_xml)
-    or die 'Error: unable to move the temporary lemma XML at ', $lemma_xml_temp, ' to ', $lemma_xml, '.';
+    or croak ('Error: unable to move the temporary lemma XML at ', $lemma_xml_temp, ' to ', $lemma_xml, '.');
 }
 
 # If we're in paranoid mode, check that new XML is verifiable
@@ -314,7 +315,7 @@ if ($paranoid) {
     my $verifier_status = system ("verifier -c $lemma_miz > /dev/null 2>&1");
     my $verifier_exit_code = $verifier_status >> 8;
     if ($verifier_exit_code != 0 || ! -z $lemma_err) {
-      die 'Error: lemma ', $lemma, ' is not verifiable after skipping all proofs preceding it!';
+      croak ('Error: lemma ', $lemma, ' is not verifiable after skipping all proofs preceding it!');
     }
   }
 }
@@ -334,7 +335,7 @@ foreach my $lemma (keys %element_for_proposition) {
     if ($verbose) {
       print "\n";
     }
-    die 'Error: lemma ', $lemma, ' could not be minimized internally.';
+    croak ('Error: lemma ', $lemma, ' could not be minimized internally.');
   }
 }
 if ($verbose) {
@@ -360,7 +361,7 @@ foreach my $lemma (keys %element_for_proposition) {
     if ($verbose) {
       print "\n";
     }
-    die 'Error: lemma ', $lemma, ' could not be minimized.';
+    croak ('Error: lemma ', $lemma, ' could not be minimized.');
   }
 }
 if ($verbose) {
@@ -376,7 +377,7 @@ foreach my $lemma (keys %element_for_proposition) {
       my $xsltproc_absrefs_status = system ("xsltproc --output $target_xml_file $absrefs_stylesheet $source_xml_file 2>/dev/null");
       my $xsltproc_absrefs_exit_code = $xsltproc_absrefs_status >> 8;
       if ($xsltproc_absrefs_exit_code != 0) {
-	die 'Error: xsltproc did not exit cleanly when computing the absolute form of ', $article_basename, '.', $extension, '.';
+	croak ('Error: xsltproc did not exit cleanly when computing the absolute form of ', $article_basename, '.', $extension, '.');
       }
     }
   }
@@ -402,15 +403,15 @@ foreach my $lemma (keys %element_for_proposition) {
     if ($verbose) {
       print "\n";
     }
-    die 'Error: xsltproc did not exit cleanly applying the lemma-dependencies stylesheet to ', $lemma, '.';
+    croak ('Error: xsltproc did not exit cleanly applying the lemma-dependencies stylesheet to ', $lemma, '.');
   }
   my $tr_status = system ("tr -s '\n' < $lemma_deps_file_tmp > $lemma_deps_file");
   my $tr_exit_code = $tr_status >> 8;
   if ($tr_exit_code != 0) {
-    die 'Error: tr died cleaning up our junky article-internal dependency list at ', $lemma_deps_file_tmp, '.';
+    croak ('Error: tr died cleaning up our junky article-internal dependency list at ', $lemma_deps_file_tmp, '.');
   }
   unlink $lemma_deps_file_tmp
-    or die 'Error: unable to delete our temprary junky article-internal dependency list at ', $lemma_deps_file_tmp, '.';
+    or croak ('Error: unable to delete our temprary junky article-internal dependency list at ', $lemma_deps_file_tmp, '.');
   my @lemma_deps = `cat $lemma_deps_file | sort -u`;
   chomp @lemma_deps;
   foreach my $lemma_dep (@lemma_deps) {
@@ -429,10 +430,10 @@ foreach my $lemma (keys %element_for_proposition) {
     if ($verbose) {
       print "\n";
     }
-    die 'Error: xsltproc did not exit cleanly applying aid-rewriting stylesheet to ', $lemma, '.';
+    croak ('Error: xsltproc did not exit cleanly applying aid-rewriting stylesheet to ', $lemma, '.');
   }
   move ($lemma_abs_xml_tmp, $lemma_abs_xml)
-    or die 'Error: unable to rename ', $lemma_abs_xml_tmp, ' to ', $lemma_abs_xml, '.';
+    or croak ('Error: unable to rename ', $lemma_abs_xml_tmp, ' to ', $lemma_abs_xml, '.');
 }
 
 # Inferred constructors
@@ -448,15 +449,15 @@ foreach my $lemma (keys %element_for_proposition) {
     if ($verbose) {
       print "\n";
     }
-    die 'Error: xsltproc did not exit cleanly applying the inferred-constructors stylesheet to ', $lemma, '.';
+    croak ('Error: xsltproc did not exit cleanly applying the inferred-constructors stylesheet to ', $lemma, '.');
   }
   my $sort_status = system ("sort -u $lemma_deps_file_tmp > $lemma_deps_file");
   my $sort_exit_code = $sort_status >> 8;
   if ($sort_exit_code != 0) {
-    die 'Error: sort did not exit cleanly when sorting the inferred constructor list.';
+    croak ('Error: sort did not exit cleanly when sorting the inferred constructor list.');
   }
   unlink $lemma_deps_file_tmp
-    or die 'Error: unable to delete the temporary inferred constructors file.';
+    or croak ('Error: unable to delete the temporary inferred constructors file.');
   my @lemma_deps = `cat $lemma_deps_file`;
   chomp @lemma_deps;
   foreach my $lemma_dep (@lemma_deps) {
@@ -483,7 +484,7 @@ foreach my $lemma (keys %element_for_proposition) {
     if ($verbose) {
       print "\n";
     }
-    die 'Error: xsltproc did not exit cleanly applying the external (envrironment) dependencies stylesheet to ', $lemma, '.';
+    croak ('Error: xsltproc did not exit cleanly applying the external (envrironment) dependencies stylesheet to ', $lemma, '.');
   }
   my @lemma_deps = `cat $lemma_deps_file`;
   chomp @lemma_deps;
