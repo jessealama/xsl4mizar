70cd828f69f03d756d3fcce70ab1ef13ec1b59fe
Jesse Alama <jesse.alama@gmail.com>
2012-01-17 18:12:48 +0000

    Check for XML validity.

--- items/inferred-constructors.pl
+++ items/inferred-constructors.pl
@@ -5,6 +5,19 @@ use strict;
 use File::Basename qw(dirname basename);
 use Getopt::Long;
 use Pod::Usage;
+use XML::LibXML;
+
+my $xml_parser = XML::LibXML->new (suppress_warnings => 1,
+				   suppress_errors => 1);
+
+sub ensure_valid_xml_file {
+  my $xml_path = shift;
+  if (defined eval { $xml_parser->parse_file ($xml_path) }) {
+    return 1;
+  } else {
+    croak ('Error: ', $xml_path, ' is not a well-formed XML file.');
+  }
+}
 
 my $help = 0;
 my $man = 0;
@@ -39,16 +52,23 @@ unless (-r $absrefs_stylesheet) {
   exit 1;
 }
 
+if  (! -e $article_xml) {
+  croak ('Error: the semantic XML for ', $article_basename, ' could not be found at the expected location (', $article_xml, ').', "\n");
+}
+
+if  (! -r $article_xml) {
+  croak ('Error: the semantic XML for ', $article_basename, ' at (', $article_xml, ') is unreadable.', "\n");
+}
+
+ensure_valid_xml_file ($article_xml);
+
 if (! -e $article_absolute_xml) {
-  if (! -e $article_xml) {
-    die 'Error: the necessary absolute form of the semantic XML for ', $article_basename, ' could not be found at the expected location (', $article_absolute_xml, '), so we tried to create it;', "\n", 'but we are unable to do so, since the semantic XML form of ', $article_basename, ' also does not exist at the expected location (', $article_xml, ').';
-  } else {
-    my $xsltproc_status = system ("xsltproc $absrefs_stylesheet $article_xml > $article_absolute_xml 2> /dev/null");
-    my $xsltproc_exit_code = $xsltproc_status >> 8;
-    if ($xsltproc_exit_code != 0) {
-      die 'Error: the needed absolute form of the XML for ', $article_basename, ' was missing, so we tried to create it;', "\n", 'but xsltproc did not exit cleanly applying the absolutizer stylesheet to ', $article_xml, '.';
-    }
+  my $xsltproc_status = system ("xsltproc $absrefs_stylesheet $article_xml > $article_absolute_xml 2> /dev/null");
+  my $xsltproc_exit_code = $xsltproc_status >> 8;
+  if ($xsltproc_exit_code != 0) {
+    die 'Error: the needed absolute form of the XML for ', $article_basename, ' was missing, so we tried to create it;', "\n", 'but xsltproc did not exit cleanly applying the absolutizer stylesheet to ', $article_xml, '.';
   }
+  ensure_valid_xml_file ($article_absolute_xml);
 }
 
 if (! -r $article_absolute_xml) {
