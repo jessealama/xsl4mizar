11162d33e1a8e5b048e17d4d7feb15b754d9ca79
Jesse Alama <jesse.alama@gmail.com>
2012-01-24 00:45:14 +0000

    Flesh out the Xsltproc module.
    It's simple, but seems to work.

--- items/xsltproc.pm
+++ items/xsltproc.pm
@@ -1,48 +1,58 @@
 package Xsltproc;
 
-our @EXPORT = qw(apply_stylesheet);
+use base qw(Exporter);
+use warnings;
+use strict;
+use Carp qw(croak);
+use IPC::Run qw(run);
+
+our @EXPORT_OK = qw(apply_stylesheet);
 
 sub apply_stylesheet {
-  my $stylesheet = shift;
-  my %parameters = %{shift ()};
-  my $document = shift;
-  my $result_path = shift;
-  my $error_path = shift;
+    my $stylesheet = shift;
+    my $document = shift;
+    my $result_path = shift;
+    my $parameters_ref = shift;
 
-  my $params = '';
-  foreach my $parameter (keys %parameters) {
-    my $value = $parameters{$parameter};
-    $params .= "--stringparam '${parameter}' '${value}' ";
-  }
+    my %parameters = defined $parameters_ref ? %{$parameters_ref}
+	                                     : ()
+					     ;
 
-  my $xsltproc_invocation
-    = defined $result_path
-      ? (defined $error_path
-	 ? "xsltproc $params $stylesheet $document > $result_path 2> $error_path"
-	 : "xsltproc $params $stylesheet $document > $result_path 2> /dev/null")
-      : (defined $error_path
-	 ? "xsltproc $params $stylesheet $document > /dev/null 2> $error_path"
-	 : "xsltproc $params $stylesheet $document > /dev/null 2> /dev/null");
-
-  my $xsltproc_status = system ($xsltproc_invocation);
-  my $xsltproc_exit_code = $xsltproc_status >> 8;
-
-  if ($xsltproc_exit_code != 0) {
-    if (defined $result_path) {
-      if (defined $error_path) {
-	croak ('Error: xsltproc did not exit cleanly applying the stylesheet at ', "\n", "\n", '  ', $stylesheet, "\n", "\n", 'to the document at', "\n", "\n", '  ', $document, ' .', "\n", "\n", 'with the parameters', "\n", "\n", '  ', $params, "\n", "\n", 'Its exit code was ', $xsltproc_exit_code, '.  The standard output and standard error streams were saved to', "\n", "\n", '  ', $result_path, "\n", "\n", 'and', "\n", "\n", '  ', $error_path, ' ,', "\n", "\n", 'respectively.');
-      } else {
-	croak ('Error: xsltproc did not exit cleanly applying the stylesheet at ', "\n", "\n", '  ', $stylesheet, "\n", "\n", 'to the document at', "\n", "\n", '  ', $document, ' .', "\n", "\n", 'with the parameters', "\n", "\n", '  ', $params, "\n", "\n", 'Its exit code was ', $xsltproc_exit_code, '.  The standard output was saved to', "\n", "\n", '  ', $result_path, "\n", "\n", 'but the standard error output was not saved.');
-      }
-    } else {
-      if (defined $error_path) {
-	croak ('Error: xsltproc did not exit cleanly applying the stylesheet at ', "\n", "\n", '  ', $stylesheet, "\n", "\n", 'to the document at', "\n", "\n", '  ', $document, ' .', "\n", "\n", 'with the parameters', "\n", "\n", '  ', $params, "\n", "\n", 'Its exit code was ', $xsltproc_exit_code, '.  The standard output was not saved, but the standard error streams were saved to', "\n", "\n", '  ', $error_path, ' .');
-      } else {
-	croak ('Error: xsltproc did not exit cleanly applying the stylesheet at ', "\n", "\n", '  ', $stylesheet, "\n", "\n", 'to the document at', "\n", "\n", '  ', $document, ' with the parameters', "\n", "\n", '  ', $params, ' .', "\n", "\n", 'Its exit code was ', $xsltproc_exit_code, '.  The standard output and standard error streams were not saved.');
-      }
+    my @xsltproc_call = ('xsltproc');
+    my $params = '';
+    foreach my $parameter (keys %parameters) {
+	my $value = $parameters{$parameter};
+	push (@xsltproc_call, '--stringparam', $parameter, "'${value}'");
     }
-  }
-  return 1;
+    push (@xsltproc_call, $stylesheet);
+    push (@xsltproc_call, $document);
+
+    my $xsltproc_out = '';
+    my $xsltproc_err = '';
+
+    my $xsltproc_result = run (\@xsltproc_call,
+			       '>',
+			       defined $result_path ? $result_path
+				                    : \$xsltproc_out
+						    ,
+			       '2>',
+			       \$xsltproc_err);
+
+    if ($xsltproc_result == 0) {
+	croak ('Error: xsltproc did not exit cleanly when applying the stylesheet ', $stylesheet, ' to ', $document, '.  Here is the error output: ', "\n", $xsltproc_err);
+    } else {
+	if (defined $result_path) {
+	    return 1;
+	} else {
+	    if (wantarray) {
+		chomp $xsltproc_out;
+		return split (/\n/, $xsltproc_out);
+	    } else {
+		return $xsltproc_out;
+	    }
+	}
+    }
+
 }
 
 1; # I'm OK, you're OK
